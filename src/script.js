6aaad632c6d65f14d307090ae4496cf81eb5f624
Ibson Barboza de Oliveira <128850394+IbsonBarboza@users.noreply.github.com>
2024-02-05 11:48:07 -0500

    js

---
+++ src/script.js
@@ -0,0 +1,25 @@
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    const top = window.scrollY;
+
+    sections.forEach(sec => {
+        const offset = sec.offsetTop - 100;
+        const height = sec.offsetHeight;
+        const id = sec.getAttribute('id');
+
+        const isInsideSection = top >= offset && top < offset + height;
+
+        navLinks.forEach(link => {
+            if (link.getAttribute('href').includes(id)) {
+                link.classList.toggle('active', isInsideSection);
+            } else {
+                link.classList.remove('active');
+            }
+        });
+    });
+
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', top > 100);
+};
